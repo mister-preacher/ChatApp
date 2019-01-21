9f15b36424ecda4144940061d8d446f3573c5311
Alex <aleksandarpopovic1221@gmail.com>
2019-01-21 14:11:29 +0100

    Initial Commit

---
+++ iChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iChat
+//
+//  Created by Aleksandar Popovic on 1/21/19.
+//  Copyright © 2019 Aleksandar Popovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
